104d8ee5023efd5a08675e845d3f2aae5341ffe1
Shekhar Barmavath <playwright.shekhar@outlook.com>
2026-05-02 19:01:53 +1000

    second message

---
+++ javascript-training/language-basics/hello-world.js
@@ -0,0 +1,8 @@
+ console.log("Hello world!")
+console.error("Error")
+console.warn("warning")
+console.info("Information")
+
+
+console
+
---
+++ javascript-training/language-basics/operators.js
@@ -0,0 +1,37 @@
+// // Declaration Variable = Data ;
+// // //arithmetic opeartors
+// let a = 10;
+// let b ="10";
+
+// console.log(a===b);
+
+// // a*=10;//a= a*10;
+
+// // let b = 20;
+
+// // // use and increment for a++ 
+// // // // increment and use ++a
+// // console.log(a);
+// // // console.log(++a);
+// // // console.log(++b);
+// // // console.log("Next line of post increment is " + (a));
+
+// // // ;
+
+// // // ++a;
+
+// // // a--;
+// // // --a;
+
+
+
+// // Assigment opeartors: (=, +=, =-, *=, /=)
+
+
+
+
+let age = 17.01;
+
+let result = (age > 17) ? "You are eligible to Vote " : "You are not not eligile to vote";
+
+console.log(result);
