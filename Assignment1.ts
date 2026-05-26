0be15f6187c320b46e54e1f2134c1ef5ce1c0098
Shekhar <playwright.shekhar@gmail.com>
2026-05-26 19:30:44 +1000

    Adding Assignment1.ts

---
+++ Assignment1.ts
@@ -0,0 +1,68 @@
+//Assignment 1
+//1. Temperature of a city in degrees Celsius: 25.5
+let temp: number = 25.5;
+console.log(`Temperature of a city in degrees Celsius: ${temp} °C`);
+//2. Whether a customer has placed an order: true or false
+let cust: boolean = false;
+console.log(`Whether a customer has placed an order: ${cust}`);
+//3. Person's phone number: "123-456-7890"
+let phone: string = "123-456-7890";
+console.log(`Person's phone number: "${phone}"`);
+//4. Amount of money in a customer's bank account: 1000.50
+let balance: number = 1000.50;
+console.log(`Amount of money in a customer's bank account: ${balance}`);
+//5. Person's email address: "john.doe@example.com"
+let email: string = "john.doe@example.com"
+console.log(`My Person's email address: "${email}"`)
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let coordinates: number[] =[37.7749, -122.4194];
+console.log(`Coordinates of a location (latitude, longitude): ${coordinates}`);
+//7. Person's marital status: true or false
+let mStatus: boolean = true;
+console.log(`Person's marital status: ${mStatus}`);
+//8. Person's occupation: "Software Engineer"
+let occupation : string = "Software Engineer"
+console.log(`Person's occupation: "${occupation}"`)
+//9. Person's favourite colour: "Blue"
+let fColour: string ="Blue";
+console.log(`Person's favourite colour: ${fColour}`);
+//10.Current year: 2023
+let cYear: number = 2023;
+console.log(`Current year: ${cYear}`);
+//11.Number of followers on a social media platform: 1,000,000
+let followers: string = "1,000,000;"
+console.log(`Number of followers on a social media platform: ${followers}`);
+//12.Rating of a movie: 7.5
+let rating: number = 7.5
+console.log(`Rating of a movie: ${rating}`);
+//13.Person's blood type: 'A'
+let bType: string = 'A';
+console.log(`Person's blood type: '${bType}'`)
+//14.Title of a book: "To Kill a Mockingbird"
+let tBook: string = "To Kill a Mockingbird";
+console.log(`Title of a book: "${tBook}"`);
+//15.Number of employees in a company: 500
+const numofEmployees: number = 500;
+console.log(`Number of employees in a company: ${numofEmployees}`);
+//16.Time of an event: 2:30 PM
+const timeOfEvent: string = "2:30 PM";
+console.log(`Time of an event: ${timeOfEvent}`);
+//17.Name of a country: "United States"
+let country: string ="United States"
+console.log(`Name of a country: "${country}"`);
+//18.Person's eye color: "Brown"
+let eColour: string ="Brown";
+console.log(`Person's eye color: "${eColour}"`);
+//19.Person's birthplace: "New York City"
+let birthplace: string ="New York City";
+console.log(`Person's birthplace: "${birthplace}"`);
+//20. Distance between two cities: 200.5
+let distance: number = 200.5;
+console.log(`Distance between two cities: ${distance}`);
+const age: number=43;
+console.log(`My age is: "${age}"`);
+
+
+
+
+
